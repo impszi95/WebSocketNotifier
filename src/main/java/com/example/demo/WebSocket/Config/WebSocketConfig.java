e75ac1284765755da99c814c46ab0463bf82223a
Varga Bence <impszi95@gmail.com>
2021-01-21 20:05:25 +0100

    traefik

--- src/main/java/com/example/demo/WebSocket/Config/WebSocketConfig.java
+++ src/main/java/com/example/demo/WebSocket/Config/WebSocketConfig.java
@@ -16,6 +16,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     }
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/gs-guide-websocket").setAllowedOrigins("http://impszi.ml").withSockJS();
+        registry.addEndpoint("/gs-guide-websocket").withSockJS();
     }
 }
