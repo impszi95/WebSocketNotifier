c201d581f0decd75ace5056e31ddf4a15aaf3bb9
Varga Bence <impszi95@gmail.com>
2021-01-27 17:57:04 +0100

    refactor

--- src/main/java/com/example/demo/WebSocket/Config/WebSocketConfig.java
+++ src/main/java/com/example/demo/WebSocket/Config/WebSocketConfig.java
@@ -17,6 +17,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         //registry.addEndpoint("/gs-guide-websocket").setAllowedOrigins("http://localhost:8080").withSockJS();
-        registry.addEndpoint("/gs-guide-websocket").setAllowedOrigins("https://impszi.ml").withSockJS();
+        registry.addEndpoint("/gs-guide-websocket").setAllowedOrigins("https://plurenter.ml").withSockJS();
     }
 }
