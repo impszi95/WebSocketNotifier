920a628d5b94342b8bc2abf82812b730b8d1750d
Varga Bence <impszi95@gmail.com>
2021-02-26 12:39:46 +0100

    allowedOrigins extended

--- src/main/java/com/example/demo/WebSocket/Config/WebSocketConfig.java
+++ src/main/java/com/example/demo/WebSocket/Config/WebSocketConfig.java
@@ -17,6 +17,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         //registry.addEndpoint("/gs-guide-websocket").setAllowedOrigins("http://localhost:8080").withSockJS();
-        registry.addEndpoint("/gs-guide-websocket").setAllowedOrigins("https://plurenter.ml").withSockJS();
+        registry.addEndpoint("/gs-guide-websocket")
+                .setAllowedOrigins("https://plurenter.ml","https://www.plurenter.ml").withSockJS();
     }
 }
